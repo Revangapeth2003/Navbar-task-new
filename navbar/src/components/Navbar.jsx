db382d02f7ec3d1651010625bb98dacfec5c881e
Revangapeth2003 <Revangapeth@gmail.com>
2025-01-18 10:19:37 +0530

    First commit

---
+++ navbar/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Corrected the toggle function
+  const ToggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <div className="container flex flex-col md:flex-row justify-between p-8 bg-slate-300 min-w-full">
+      <div>
+        <h1 className="text-2xl font-bold">Header</h1>
+      </div>
+      <div>
+        <button
+          onClick={ToggleMenu}
+          className="absolute top-2 right-2 md:hidden"
+        >
+          &#9776;
+        </button>
+      </div>
+      <div>
+        <ul
+          className={`${isMenuOpen ? "block" : "hidden"} 
+        flex flex-col md:flex-row  `}
+        >
+          <li className="mr-6">Home</li>
+          <li className="mr-6">Contact</li>
+          <li className="mr-6">About</li>
+          <li className="mr-6">SignIn</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
